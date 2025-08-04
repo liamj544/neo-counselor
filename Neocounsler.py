afaa80d0a486477aec1718c984bcfffc60133a56
liamj544 <liamjackson.baby6@gmail.com>
2025-08-04 12:19:16 -0700

    Delete Neocounsler.py

--- Neocounsler.py
+++
@@ -1,4 +0,0 @@
- import html
-
-html_content = '<div id="deployment-eb695328-3168-4eff-9b51-1316a2e0d733"></div>\n<script src="https://studio.pickaxe.co/api/embed/bundle.js" defer></script>'
-print(html.unescape(html_content))
